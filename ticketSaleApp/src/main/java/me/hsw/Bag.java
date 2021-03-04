474398bdb73d46efd5e130517f09a570559989ab
hsw0905 <hsw0905@gmail.com>
2021-03-05 00:37:17 +0900

    refactor: Bag, TicketOffice 캡슐화

--- ticketSaleApp/src/main/java/me/hsw/Bag.java
+++ ticketSaleApp/src/main/java/me/hsw/Bag.java
@@ -15,7 +15,19 @@ public class Bag {
         this.invitation = invitation;
     }
 
-    public boolean hasInvitation() {
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        }
+        else {
+            setTicket(ticket);
+            minusAmount(ticket.getFree());
+            return ticket.getFree();
+        }
+    }
+
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
@@ -23,15 +35,15 @@ public class Bag {
         return ticket != null;
     }
 
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount) {
+    private void plusAmount(Long amount) {
         this.amount += amount;
     }
 }
